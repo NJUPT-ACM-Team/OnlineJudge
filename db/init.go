ef005b65487b20c90a422b6d60d59b35ee3f974a
kevince <prikevs@gmail.com>
2016-11-17 01:51:07 +0800

    Add parseTime

--- db/init.go
+++ db/init.go
@@ -1,11 +1,14 @@
 package db
 
 func Init() {
+	params := make(map[string]string)
+	params["parseTime"] = "true"
 	mysql_config := MySQLConfig{
 		drivername: "mysql",
 		username:   "test",
 		dbname:     "ojtest",
 		password:   "abc123",
+		params:     params,
 	}
 	var config Config
 	config = &mysql_config
