b81175a5ad8a12e00025eb3dd9c53e25c89b6dbc
kevince <prikevs@gmail.com>
2016-11-26 18:54:13 +0800

    TODO: login.go

--- handler/api/responses.go
+++ handler/api/responses.go
@@ -13,3 +13,17 @@ func NewSubmitResponseError(debug bool, code int32, err error) *SubmitResponse {
 		Error: theerror,
 	}
 }
+
+func NewLoginValidationResponseError(debug bool, code int32, err error) *LoginValidationResponse {
+	errmsg := "Wrong username or password."
+	if debug == true && err != nil {
+		errmsg = err.Error()
+	}
+	theerror := &Error{
+		Code: code,
+		Msg:  errmsg,
+	}
+	return &LoginValidationResponse{
+		Error: theerror,
+	}
+}
--- handler/login.go
+++ handler/login.go
@@ -13,6 +13,17 @@ func (this *Handler) LoginInit(li *api.LoginInitRequest) *api.LoginInitResponse 
 }
 
 func (this *Handler) LoginValidation(li *api.LoginValidationRequest) *api.LoginValidationResponse {
+	if err := this.OpenDB(); err != nil {
+		return api.NewSubmitResponseError(this.debug, 500, err)
+	}
+	defer this.CloseDB()
+
+	// Validate the login information
+	um := NewUserModel()
+	r, err := um.Validate(this.tx, li.GetUsername(), li.GetPasswd())
+	if err != nil {
+
+	}
 
 	return nil
 }
