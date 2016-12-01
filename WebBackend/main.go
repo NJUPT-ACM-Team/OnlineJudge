ddf5e5a42634b0dda877d6c32ccc09ebb23af743
kevince <prikevs@gmail.com>
2016-12-01 20:30:43 +0800

    delete binary, simple web

--- WebBackend/main.go
+++ WebBackend/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"OnlineJudge/WebBackend/routers"
 
+	"github.com/gorilla/context"
+
 	"net/http"
 )
 
 func main() {
 	router := routers.Init()
 	// http.Handle("/", router)
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8000", context.ClearHandler(router))
 }
